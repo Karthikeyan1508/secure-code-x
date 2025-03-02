e19451cb0c0cf3e5675a58bb49b3ffdafb655205
Karthikeyan1508 <karthikeyan.36947@gmail.com>
2025-03-02 08:12:15 +0530

    deployment issues

--- app.js
+++ app.js
@@ -15,18 +15,11 @@ app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY, // Fix newline formatting
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-};
+const serviceAccount = require('./serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
